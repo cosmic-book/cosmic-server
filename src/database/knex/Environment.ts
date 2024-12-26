faf318bff2bc024c74ae4a8e996e472c32b96cf5
Fernando Halmenschlager Filter <fernando.filter@universo.univates.br>
2024-12-26 08:19:38 -0300

    Update Environment.ts

--- src/database/knex/Environment.ts
+++ src/database/knex/Environment.ts
@@ -1,15 +1,18 @@
 import { Knex } from 'knex';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const development: Knex.Config = {
   client: 'mysql2',
   useNullAsDefault: true,
   connection: {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'cosmic-db',
+    host: process.env.DB_HOST || '',
+    port: Number(process.env.DB_PORT) || 0,
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || '',
     charset: 'utf8'
   },
   migrations: {
